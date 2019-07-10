8d3db23093e77a8730222b67c391cd7cd3f2871f
Tyler Jennings <tjennings5@outlook.com>
2019-07-10 15:27:23 -0500

    adding the homescreen xcode project

---
+++ home screenTests/home_screenTests.swift
@@ -0,0 +1,34 @@
+//
+//  home_screenTests.swift
+//  home screenTests
+//
+//  Created by Tyler Jennings on 7/10/19.
+//  Copyright © 2019 Tyler Jennings. All rights reserved.
+//
+
+import XCTest
+@testable import home_screen
+
+class home_screenTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
